0a2067c82cca98e4529fd6a082491f1dbd1ecb68
Jonathan Bodrero <jbodrero@hotmail.com>
2025-05-21 10:28:52 -0600

    Step 1 Explore integer math

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Jonathan Bodrero  May 21, 2025
+//  Lab 1
+
+//Explore Integer Math
+int a = 18;
+int b = 26;
+int c = b / a;
+Console.WriteLine(c);
